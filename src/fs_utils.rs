dc520284dc5ad583b8ecca2b0e581463054f37d2
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-03-23 23:50:45 +0530

    Initialize the .git directory

--- src/fs_utils.rs
+++ src/fs_utils.rs
@@ -1,5 +1,9 @@
-use std::{fs, path::PathBuf};
+use std::fs;
 
-pub fn get_absolut_path(path: &str) -> Result<PathBuf, std::io::Error> {
-    fs::canonicalize(path)
+// pub fn get_absolut_path(path: &str) -> Result<PathBuf, std::io::Error> {
+//     fs::canonicalize(path)
+// }
+
+pub fn create_folder(path: &str) -> Result<(), std::io::Error> {
+    fs::create_dir(path)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
+use std::{env, fs::File, io::Write};
+
 mod fs_utils;
 
 fn main() {
-    let absolut_path = fs_utils::get_absolut_path("test/test.txt").unwrap();
+    let argument: Vec<String> = env::args().collect();
 
-    println!("{:?}", absolut_path);
+    if argument.len() > 1 && argument[1] == "init" {
+        println!("Initializing Git Reposistory");
+
+        fs_utils::create_folder("test").expect("Error in creating root folder");
+
+        let root = "test/.git";
+
+        fs_utils::create_folder(root).expect("Error in creating root folder");
+
+        let list_of_folders: [&str; 2] = ["/objects", "/refs"];
+
+        for folder in list_of_folders {
+            let folder_path = format!("{}{}", root, folder);
+            let error_message = format!("Error in creating folder: {}", folder_path);
+
+            fs_utils::create_folder(&folder_path).expect(&error_message);
+
+            println!("{}", folder);
+        }
+
+        // create a file
+        let file_name = format!("{}/HEAD", root);
+
+        let mut file = File::create(file_name).expect("Error in creating file");
+
+        file.write_all(b"ref: refs/heads/main\n")
+            .expect("Error in writting data");
+    } else {
+        println!("Incorrect argument")
+    }
 }
